311829cafff787338c334ebe49fef15535fc05a8
Ty Thorsen <ty.thorsen@ave81.com>
2015-10-29 18:24:24 -0500

    add vendor js

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,17 +3,28 @@ var gulp   = require('gulp'),
     concat = require('gulp-concat'),
     jshint = require('gulp-jshint'),
     size   = require('gulp-filesize'),
-    uglify = require('gulp-uglify');;
+    uglify = require('gulp-uglify');
+    vendor = config.src + '/vendor';
 
 gulp.task('scripts', function() {
-  return gulp.src([
-      config.src+'/britannia.js'
-    ])
+  gulp.src([
+    config.src+'/britannia.js'
+  ])
     .pipe(concat('britannia.js'))
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(size())
     .pipe(uglify())
+    .pipe(size())
+    .pipe(gulp.dest(config.dist));
+
+  gulp.src([
+    vendor + '/angular-1.4.7.min.js',
+    vendor + '/angular-animate-1.4.7.min.js',
+    vendor + '/angular-aria-1.4.7.min.js',
+    vendor + '/angular-material-1.0.0-rc1.min.js'
+  ])
+    .pipe(concat('vendor.js'))
     .pipe(size())
     .pipe(gulp.dest(config.dist));
 });

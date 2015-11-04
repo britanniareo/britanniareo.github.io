430ea3034b7f277eabf5e33d8ed1d2176c8f6dec
Ty Thorsen <ty.thorsen@ave81.com>
2015-11-04 13:59:35 -0600

    set up temporary homepage

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,14 +2,14 @@ var gulp      = require('gulp');
 var browserSync = require('browser-sync').create();
 
 gulp.task('watch', ['build'], function() {
-  browserSync.init({
+  /*browserSync.init({
     server: "./"
-  });
+  });*/
 
-  gulp.watch("./src/scss/**/*.scss", ['sass']);
-  gulp.watch("./src/js/**/*.js", ['scripts']);
-  gulp.watch("./src/img/**/*", ['images']);
-  gulp.watch("./src/html/**/*", ['html']);
-  gulp.watch("./index.html").on('change', browserSync.reload);
-  gulp.watch("./dist/*/**").on('change', browserSync.reload);
+  gulp.watch("./static/src/scss/**/*.scss", ['sass']);
+  gulp.watch("./static//src/js/**/*.js", ['scripts']);
+  gulp.watch("./static//src/img/**/*", ['images']);
+  gulp.watch("./static//src/html/**/*", ['html']);
+  //gulp.watch("./index.html").on('change', browserSync.reload);
+  //gulp.watch("./dist/*/**").on('change', browserSync.reload);
 });
